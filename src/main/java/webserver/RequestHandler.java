95da60cb0ac7249c5c4dd3d1253db7520d34b3a7
sangwon <swpark@smartauth.kr>
2017-12-26 10:23:26 +0900

    ver 1.0

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,9 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -23,13 +25,28 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
+            // 1 test .
+            // header 가져오기.
+
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
             String line = br.readLine();
             if(line == null){
                 return;
             }
-
+            // 2 test.
             String url = HttpRequestUtils.getUrl(line);
+            if(url.startsWith("/create")){
+                int index = url.indexOf("?");
+                String requestPath = url.substring(0, index);
+                String queryString = url.substring(index+1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user);
+                url = "/index.html";
+            }
+
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
