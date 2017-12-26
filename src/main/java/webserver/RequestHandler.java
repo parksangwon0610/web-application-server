c2b6ab7d39fc94e13787d9753ed1dbb5ff862969
sangwon <swpark@smartauth.kr>
2017-12-26 11:22:39 +0900

    ver 3.4.3

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -34,17 +34,21 @@ public class RequestHandler extends Thread {
             if(line == null){
                 return;
             }
-            // 2 test.
             String url = HttpRequestUtils.getUrl(line);
-            if(url.startsWith("/create")){
-                int index = url.indexOf("?");
-                String requestPath = url.substring(0, index);
-                String queryString = url.substring(index+1);
-                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
-                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
-                log.debug("User : {}", user);
-                url = "/index.html";
-            }
+
+            // 2 get test.
+
+//            if(url.startsWith("/create")){
+//                int index = url.indexOf("?");
+//                String queryString = url.substring(index+1);
+//                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+//                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+//                log.debug("User : {}", user);
+//                url = "/index.html";
+//            }
+
+            // 3. post test
+
 
 
             DataOutputStream dos = new DataOutputStream(out);
